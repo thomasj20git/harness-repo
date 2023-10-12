9c8918679f7ce4efdc89c289345dc963bf493ccd
Thomas Johnson <thomasj.johnson20@gmail.com>
2023-10-12 16:36:30 -0700

    test2

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1" # Modify with your desired AWS region
 }
 resource "aws_s3_bucket" "example" {
-  bucket = "harness-bucket-practice"  # Replace with your desired bucket name
+  bucket = "harness-bucket-practice-2"  # Replace with your desired bucket name
 
   acl    = "private"  # You can adjust the ACL (Access Control List) as needed
 

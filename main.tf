f404d5980d59928045237bf77e8bd7bab76ff576
Thomas Johnson <thomasj.johnson20@gmail.com>
2023-10-12 16:30:13 -0700

    add main

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1" # Modify with your desired AWS region
+}
+resource "aws_s3_bucket" "example" {
+  bucket = "harness-bucket-practice"  # Replace with your desired bucket name
+
+  acl    = "private"  # You can adjust the ACL (Access Control List) as needed
+
+  tags = {
+    Name = "My Example S3 Bucket"
+  }
+}

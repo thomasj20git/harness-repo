59d0ec2672531e0f45c3d70e6bde475b4891b3ab
Thomas Johnson <thomasj.johnson20@gmail.com>
2023-10-13 07:58:13 -0700

    test

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-terraform {
-  backend "http" {
-    address = "${harness_workspace.variable.TF_HTTP_ADDRESS}"
-    username = "harness"
-    password = "${harness_workspace.variable.TF_HTTP_PASSWORD}"
-    lock_address = "${harness_workspace.variable.TF_HTTP_UNLOCK_ADDRESS}"
-    lock_method = "POST"
-    unlock_address = "${harness_workspace.variable.TF_HTTP_UNLOCK_ADDRESS}"
-    unlock_method = "DELETE"
-  }
-}
+# terraform {
+#   backend "http" {
+#     address = "${harness_workspace.variable.TF_HTTP_ADDRESS}"
+#     username = "harness"
+#     password = "${harness_workspace.variable.TF_HTTP_PASSWORD}"
+#     lock_address = "${harness_workspace.variable.TF_HTTP_UNLOCK_ADDRESS}"
+#     lock_method = "POST"
+#     unlock_address = "${harness_workspace.variable.TF_HTTP_UNLOCK_ADDRESS}"
+#     unlock_method = "DELETE"
+#   }
+# }
 provider "aws" {
   region = "us-east-1" # Modify with your desired AWS region
 }

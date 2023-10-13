4ad3775af68c6074fee78045994c9a39fc07c598
Thomas Johnson <thomasj.johnson20@gmail.com>
2023-10-12 18:49:28 -0700

    backend

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
 provider "aws" {
   region = "us-east-1" # Modify with your desired AWS region
 }
+backend "http" {
+    address = TF_HTTP_ADDRESS
+    username = "harness"
+    password = TF_HTTP_PASSWORD
+    lock_address = TF_HTTP_UNLOCK_ADDRESS
+    lock_method = "POST"
+    unlock_address = TF_HTTP_UNLOCK_ADDRESS
+    unlock_method = "DELETE"
+  }
 resource "aws_s3_bucket" "example" {
   bucket = "harness-bucket-practice-2"  # Replace with your desired bucket name
 

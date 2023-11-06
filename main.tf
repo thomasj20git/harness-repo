4c21df22fc9956b7d7c7119e19e7a80d1852b5f4
Thomas Johnson <thomasj.johnson20@gmail.com>
2023-11-06 10:51:08 -0700

    required providers

--- main.tf
+++ main.tf
@@ -11,6 +11,12 @@
 # }
 terraform {
   required_version = ">= 0.12"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.0, < 4.0" # Specify the desired version constraint
+    }
+  }
 }
 provider "aws" {
   region = "us-east-1" # Modify with your desired AWS region
